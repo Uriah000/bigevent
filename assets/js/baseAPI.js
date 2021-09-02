90b5a75fd2e6c6a7b6958bc91221d8d08de6d615
Uriah <tym000212@163.com>
2021-09-02 16:15:55 +0800

    完成了主页相关功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,27 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
+    // 统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     console.log(options.url)
+
+    // 统一为有权限的接口设置根路径
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete函数
+    options.complete = function(res) {
+        console.log(res)
+            //在complete回调函数中，通过res.responseJSON拿到服务器相应回的数据
+        if (res.responseJSON.status === 1 & res.responseJSON.message === '身份认证失败！') {
+            // 1，清空本地存储中的token
+            localStorage.removeItem('token')
+                // 2.跳转到登录页
+            location.href = '/login.html'
+        }
+    }
 
 })
---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+$(function() {
+    getUserInfo()
+
+    var layer = layui.layer;
+
+    $('#btnLogout').on('click', function() {
+        layer.confirm('确认退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            // 1，清空本地存储中的token
+            localStorage.removeItem('token')
+                // 2.跳转到登录页
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        /*         headers: {
+                    Authorization: localStorage.getItem('token') || ''
+                }, */
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+
+            // 调用渲染用户头像的方法renderAvatar
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染用户头像
+function renderAvatar(user) {
+    // 1.获取用户名
+    var name = user.nickname || user.username
+
+    // 2.渲染欢迎词
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+    // 3.按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+    } else {
+        // 渲染用户头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
